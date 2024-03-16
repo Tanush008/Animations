f86979723609025ca3dd2930c805d872f9ff5e60
Tanush008 <149870803+Tanush008@users.noreply.github.com>
2024-03-16 17:03:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+function firstPageAnim() {
+    var tl = gsap.timeline();
+    tl.from(".nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.4,
+        ease: Expo.easeInOut
+    })
+        .to(".bounding-elem", {
+            y: 0,
+
+            duration: 2,
+            ease: Expo.easeInOut,
+            stagger: .2,
+            delay: -1
+        })
+        .from(".hero-footer", {
+            y: '-10',
+            opacity: 0,
+            duration: 1.4,
+            delay: -1,
+            ease: Expo.easeInOut
+        })
+}
+
+function circleskew() {
+    // define default value
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0
+    window.addEventListener("mousemove", function (dets) {
+
+        xscale = gsap.utils.clamp(.8, 1.2, dets.clientX - xprev);
+        yscale = gsap.utils.clamp(.8, 1.2, dets.clientY - yprev);
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+
+        circleMouseFollower(xscale, yscale);
+    }
+    );
+}
+circleskew();
+
+function circleMouseFollower(xscale, yscale) {
+    window.addEventListener("mousemove", function (dets) {
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale},${yscale})`;
+    })
+}
+circleMouseFollower();
+firstPageAnim();
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+
+elem.addEventListener("mouseleave", function (dets) {
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+
+   
+         gsap.to(elem.querySelector("img"), {
+           opacity: 0,
+           ease: Power3, 
+        duration:0.5
+         });
+       });
+
+    elem.addEventListener("mousemove", function (dets) {
+ var diff = dets.clientY - elem.getBoundingClientRect().top;
+
+diffrot = dets.clientX - rotate;
+rotate = dets.clientX;
+
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3, 
+        top: diff,
+        left: dets.clientX,
+       rotate: gsap.utils.clamp(-20,20,diffrot *0.5)
+      });
+    });
+   });
